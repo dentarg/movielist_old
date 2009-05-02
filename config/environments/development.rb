f95c5a8616885268bf3c0efdd563110d3a8db725
Patrik Ragnarsson <patrik@starkast.net>
2009-05-02 19:33:28 +0200

    Bort install complete

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,5 +17,5 @@ config.action_controller.perform_caching             = false
 config.action_mailer.raise_delivery_errors = false
 
 # Restful Authentication
-REST_AUTH_SITE_KEY = 'f5945d1c74d3502f8a3de8562e5bf21fe3fec887'
+REST_AUTH_SITE_KEY = 'ceedbee10a7dccb034cf55cd0c2b119e'
 REST_AUTH_DIGEST_STRETCHES = 10
